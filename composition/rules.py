fe0bd82e409a0a459b2ae1e97089ceeff4926fdf
Meherzan-Gai <meherzan.gai@gmail.com>
2022-07-01 17:02:37 -0400

    Created the rules class and added some base methods

--- composition/rules.py
+++ composition/rules.py
@@ -1,10 +1,22 @@
 from sharedtone import *
 from numnotes import *
 from interval import *
-rules = {
-    "numShared": numShared,
-    "numNotes": numNotes,
-    "intervals":intervalLengths
-}
-def scoreChord(chord,previousChord):
-    pass
+
+#make new rule class
+class rules:
+    def __init__(self,intervalRule,sharedRule,numNotesRule):
+        self.intervalRule = intervalRule
+        self.sharedRule = sharedRule
+        self.numNotesRule = numNotesRule
+
+    #finds the order of priority for the rules
+    def getPriority(self):
+        pass
+
+
+
+    #creates a list of possible chords that fit the rules
+    def getPossibleChords(previousChord):
+        pass
+
+
