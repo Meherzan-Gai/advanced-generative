9564ea1a18c90936d9d6aff84a385740ea6dc3d0
Meherzan-Gai <meherzan.gai@gmail.com>
2022-07-27 19:02:32 -0400

    Made the firstchord rule markdown file

---
+++ composition/rules/firstchord.md
@@ -0,0 +1,11 @@
+
+# **First Chord Rule:**
+
+## What the rule controls:  
+
+##### The rule controls the types of chords the progression can start with. <br>
+
+## Levels:
+
+
+
--- composition/rules/firstchord.py
+++ composition/rules/firstchord.py
@@ -1,3 +1,20 @@
 from rules.base import Rule
 from chord import Chord
 from progression import Progression
+
+class FirstChord(Rule):
+    def __init__(self,level):
+        super().__init__()
+        self.ruleType = level
+
+    #returns the possible chords that fit this rule
+    def getChords(self, **kwargs):
+        pass
+
+    def ruleCheck(self, **kwargs):
+        pitchIdx = kwargs.get(pitchIdx)
+        if (pitchIdx==0):
+            return True
+
+        else:
+            return True
