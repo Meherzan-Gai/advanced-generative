2c95c85e0db38034d6ac00615e172171f1ea1ec9
Zenza235 <paoloatienza235@gmail.com>
2022-07-27 22:26:29 -0400

    implement part of note data for classification

--- audio_analysis/analyzer.py
+++ audio_analysis/analyzer.py
@@ -12,9 +12,20 @@ def get_audio_info(path, n_fft=2048):
     onsets = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr, units='frames')
     return S, freq_range, onsets
 
-def get_note_data(note, num_highest=3):
-    a = np.amax(note, axis=1)
-    return a
+def get_note_data(note, num_bins=3):
+    # list of the maximum values of each bin across the frames
+    bin_maxes = np.amax(note, axis=1)
+    # "num_bins"th highest bins ordered from least to greatest
+    bins = np.argpartition(bin_maxes, -num_bins)[-num_bins:]
+    print(bins)
+    print(bin_maxes[bins])
+
+    bin_frames = note[bins, :]
+    # average magnitude of each bin across the frames
+    bin_means = np.mean(bin_frames, axis=1)
+
+    data = np.append([bins], [bin_means], axis=0)
+    return data
 
 if __name__ == '__main__':
     import os
@@ -23,5 +34,6 @@ if __name__ == '__main__':
     S, freq_range, onsets = get_audio_info(path)
 
     note = S[:, onsets[0]:onsets[1]]
-    a = np.argpartition(get_note_data(note))
-    print(a)
+    data = get_note_data(note, num_bins=10)
+
+    print(data)
