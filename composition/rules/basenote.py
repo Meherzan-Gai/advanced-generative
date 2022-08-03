5f6d2128b1b661955ef50201f8d21eb3cf2a6a05
Meherzan-Gai <meherzan.gai@gmail.com>
2022-08-02 23:04:33 -0400

    Wrote the ruleCheck method for the basenote rule

--- composition/rules/basenote.py
+++ composition/rules/basenote.py
@@ -5,11 +5,21 @@ from progression import Progression
 class BaseNote(Rule):
     def __init__(self,level):
         super().__init__()
-        self.ruleType = level
+        if (level==1):
+            self.maxDistance = 2
+        elif (level==2):
+            self.maxDistance = 4
+        else:
+            self.maxDistance = 12
 
     #returns the possible chords that fit this rule
     def getChords(self, **kwargs):
         pass
 
     def ruleCheck(self, **kwargs):
+        prevChord = kwargs.get("prevChord", None)
+        newChord = kwargs.get("newChord", None)
+        distance = abs(prevChord.stack[0]-newChord.stack[0])
+        if (distance > self.maxDistance):
+            return False
         return True
