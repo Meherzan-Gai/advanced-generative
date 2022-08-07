9dab627f344a7811eac7c52470e2e04186071a91
Meherzan-Gai <meherzan.gai@gmail.com>
2022-08-07 12:16:12 -0400

    Made changes to voicing rule

--- composition/voicing.py
+++ composition/voicing.py
@@ -3,24 +3,57 @@ from progression import Progression
 
 class VoicingRule:
 
-    def __init__(self,shiftAmt):
+    def __init__(self,progression,pitches):
         self.shiftAmt = 12
+        self.progression = progression
+        self.pitches = pitches
 
-    def getVoicing(progression,pitches):
-        basslineProgression = VoicingRule.moveBassline(progression,pitches)
-        VoicingRule.moveNotes(basslineProgression)
+    def getVoicing(self):
+        self.setShiftAmt()
+        self.moveBassline()
+        self.moveNotes()
             
-    
-    def moveBassline(progression,pitches):
-        pass
+    def setShiftAmt(self):
+        bassNotes = []
+        for chord in self.progression.chords:
+            bassNotes.append(chord.stack[0])
+        progressionLow = VoicingRule.getLowest(bassNotes)
+        progressionHigh = VoicingRule.getHighest(bassNotes)
+        pitchLow = VoicingRule.getLowest(self.pitches)
+        pitchHigh = VoicingRule.getHighest(self.pitches)
 
-    def moveNotes(progression):
+        if (pitchLow>progressionHigh):
+            while (pitchLow>progressionHigh+12+self.shiftAmt):
+                self.shiftAmt+=12
+        
+
+    
+    def moveBassline(self,progression):
         for chord in progression.chords:
-            for x in range (1,len(chord.stack),1):
-                while (chord.stack[x] < chord.stack[0]):
-                    chord.stack[x]+=12
+            chord[0]+=self.shiftAmt
+
+
+    def moveNotes(self,progression):
+        for chord in progression.chords:
+            for noteIdx in range (1,len(chord.stack),1):
+                while (chord.stack[noteIdx] < chord.stack[0]):
+                    chord.stack[noteIdx]+=12
 
     
+    def getLowest(notes):
+        lowestNote = notes[0]
+        for idx in range(1,len(notes),1):
+            if (lowestNote>notes[idx]):
+                lowestNote = notes[idx]
+        return lowestNote
+
+    
+    def getHighest(notes):
+        highestNote = notes[0]
+        for idx in range(1,len(notes),1):
+            if (highestNote<notes[idx]):
+                highestNote = notes[idx]
+        return highestNote
 
 
 
