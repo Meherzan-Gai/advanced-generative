cc3b10a38d47900668898262f4a9bb351e68bbe1
Zenza235 <paoloatienza235@gmail.com>
2022-08-20 21:58:31 -0400

    add 'sample_id' column to data

--- audio_analysis/training_data.py
+++ audio_analysis/training_data.py
@@ -6,13 +6,16 @@ import pandas as pd
 
 import analyzer
 
+
 def format_train_data(path, filename):
     S, freq_range, onsets = analyzer.get_audio_info(path)
 
     note = S[:, onsets[0]:]
     data = analyzer.get_note_data(note, num_bins=30)
 
-    df = pd.DataFrame(data.T, columns=['frequency_bin', 'max_magnitude', 'min_magnitude', 'avg_magnitude', 'sd_magnitude', 'avg_difference', 'sd_difference'])
+    df = pd.DataFrame(data.T,
+                      columns=['frequency_bin', 'max_magnitude', 'min_magnitude', 'avg_magnitude', 'sd_magnitude',
+                               'avg_difference', 'sd_difference'])
     df.sort_values('avg_magnitude', inplace=True, ascending=False, ignore_index=True)
 
     # does not include freq bin number
@@ -26,20 +29,23 @@ def format_train_data(path, filename):
 
     return final
 
+
 def main():
     # notes in sample folders are just for testing
-    for root, dirs, files in os.walk(f'{os.getcwd()}/audio_analysis/samples'):
+    for root, dirs, files in os.walk(f'{os.getcwd()}/samples'):
         pass
     paths = list(map(lambda file: f'{root}/{file}', files))
 
     frames = []
     for i in range(len(paths)):
         frames.append(format_train_data(paths[i], files[i]))
-    
+
     result = pd.concat(frames, ignore_index=True)
+    result.insert(0, 'sample_id', range(0, len(result)))
     print(result)
 
-    result.to_csv(path_or_buf=f'{os.getcwd()}/audio_analysis/notes.csv', index=False)
+    result.to_csv(path_or_buf=f'{os.getcwd()}/notes.csv', index=False)
+
 
 if __name__ == '__main__':
     main()
