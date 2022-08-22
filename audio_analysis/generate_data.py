f9e3ea13edc34822c44d7dbe188e5e24563e699c
Zenza235 <paoloatienza235@gmail.com>
2022-08-22 16:39:27 -0400

    change name of train_model.py to generate_data.py

---
+++ audio_analysis/generate_data.py
@@ -0,0 +1,27 @@
+import os
+import librosa
+import pandas as pd
+# from sdv.lite import TabularPreset
+from sdv.tabular import GaussianCopula
+
+
+def generate_data():
+    import os
+
+    file = f'{os.getcwd()}/notes.csv'
+    note_data = pd.read_csv(file, header=0)
+
+    # TODO: add bounds and other restrictions to generate fake data more close to real data
+    # model = TabularPreset(name='FAST_ML', metadata=metadata)
+    model = GaussianCopula()
+    model.fit(note_data)
+
+    synthetic_data = model.sample(num_rows=500)
+    return synthetic_data
+
+
+if __name__ == '__main__':
+    frequencies = librosa.fft_frequencies(sr=22050, n_fft=2048)
+
+    working_df = generate_data()
+    working_df.to_csv(path_or_buf=f'{os.getcwd()}/gaussian_copula_notes.csv', index=False)
