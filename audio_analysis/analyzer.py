213c444644855337e280839c028b3f6026f4fafa
Zenza235 <paoloatienza235@gmail.com>
2022-08-24 11:05:51 -0400

    add function to get pitches w/o ML, can save MIDI numbers as JSON file

--- audio_analysis/analyzer.py
+++ audio_analysis/analyzer.py
@@ -9,8 +9,7 @@ def get_audio_info(path, n_fft=2048):
     S = np.abs(librosa.stft(y, n_fft=n_fft))
     freq_range = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
     o_env = librosa.onset.onset_strength(y=y, sr=sr, max_size=S.shape[0])
-    onsets = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr, units='frames', pre_max=3, post_max=3, pre_avg=3,
-                                        post_avg=5)
+    onsets = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr, units='frames', pre_max=3, post_max=3, pre_avg=3, post_avg=5)
 
     # onset strength graph
     # import matplotlib.pyplot as plt
@@ -24,22 +23,27 @@ def get_audio_info(path, n_fft=2048):
     return S, freq_range, onsets
 
 
-def get_note_data(note, num_bins=3):
+def get_note_data(note, n_bins, for_ML=False) -> np.ndarray:
     # list of the maximum values of each bin across the frames
     bin_maxes = np.amax(note, axis=1)
-    # "num_bins"th highest bins ordered from least to greatest
-    bins = np.argpartition(bin_maxes, range(len(bin_maxes) - num_bins, len(bin_maxes)))[-num_bins:]
+    # nth highest bins ordered from least to greatest
+    bins = np.argpartition(bin_maxes, range(len(bin_maxes) - n_bins, len(bin_maxes)))[-n_bins:]
 
-    bin_mags = note[bins, :]
-    mag_maxes = np.max(bin_mags, axis=1)
-    mag_mins = np.min(bin_mags, axis=1)
-    # average magnitude of each bin across the frames
-    mag_means = np.mean(bin_mags, axis=1)
-    mag_stds = np.std(bin_mags, axis=1)
+    bin_magnitudes = note[bins, :]
 
-    bin_diffs = np.diff(bin_mags)
-    diff_avgs = np.mean(bin_diffs, axis=1)
-    diff_stds = np.std(bin_diffs, axis=1)
+    avg_magnitudes = np.mean(bin_magnitudes, axis=1)
 
-    data = np.concatenate(([bins], [mag_maxes], [mag_mins], [mag_means], [mag_stds], [diff_avgs], [diff_stds]), axis=0)
+    if not for_ML:
+        data = np.concatenate(([bins], [avg_magnitudes]), axis=0)
+        return data.T
+
+    max_magnitudes = np.max(bin_magnitudes, axis=1)
+    min_magnitudes = np.min(bin_magnitudes, axis=1)
+    std_magnitudes = np.std(bin_magnitudes, axis=1)
+
+    bin_differences = np.diff(bin_magnitudes)
+    avg_differences = np.mean(bin_differences, axis=1)
+    std_differences = np.std(bin_differences, axis=1)
+
+    data = np.concatenate(([bins], [max_magnitudes], [min_magnitudes], [avg_magnitudes], [std_magnitudes], [avg_differences], [std_differences]), axis=0)
     return data
---
+++ audio_analysis/main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import librosa
+import json
+
+import analyzer
+
+
+def determine_pitches(melody, freq_range) -> np.ndarray:
+    result = []
+    for note in melody:
+        max_bin_index = np.argmax(note[:, 1])
+        result.append(int(note[max_bin_index, 0]))
+
+    return librosa.hz_to_midi(freq_range[result])
+
+
+def determine_pitches_ML(melody_data):
+    pass
+
+
+# if for_ML is true, it should build the full note_data df
+# should
+def analyze_audio_file(path, n_fft=2048, for_ML=False):
+    S, freq_range, onsets = analyzer.get_audio_info(path, n_fft=n_fft)
+
+    melody_data = []
+    for i in range(0, len(onsets) - 1):
+        note = S[:, onsets[i]:onsets[i + 1]]
+        note_data = analyzer.get_note_data(note, n_bins=100, for_ML=for_ML)
+        melody_data.append(note_data)
+
+    if for_ML:
+        determine_pitches_ML(melody_data)
+        raise Exception('Not implemented yet.')
+    else:
+        return determine_pitches(melody_data, freq_range)
+
+
+if __name__ == '__main__':
+    import os.path
+
+    dirname = os.path.dirname
+    PROJECT_ROOT = dirname(os.path.abspath(os.curdir))
+
+    filepath = f'{PROJECT_ROOT}/audio_files/test_audio/test_piano_old.wav'
+
+    melody = analyze_audio_file(filepath, for_ML=False).tolist()
+    print(librosa.midi_to_note(melody))
+
+    with open(f'{PROJECT_ROOT}/midi_melodies/{os.path.basename(filepath)}.json', 'w') as out:
+        json.dump(melody, out)
