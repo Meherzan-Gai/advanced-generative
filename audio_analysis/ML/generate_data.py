7e97d122c1a23a887f0db7c3ade6731ea02e79d8
Zenza235 <paoloatienza235@gmail.com>
2022-08-26 09:55:05 -0400

    create csv files based on new note samples

--- audio_analysis/ML/generate_data.py
+++ audio_analysis/ML/generate_data.py
@@ -1,18 +1,13 @@
 import os
-import librosa
 import pandas as pd
-# from sdv.lite import TabularPreset
 from sdv.tabular import GaussianCopula
 
 
-def generate_data():
-    import os
+def generate_data(note_data):
 
-    file = f'{os.getcwd()}/notes.csv'
-    note_data = pd.read_csv(file, header=0)
-
-    # TODO: add bounds and other restrictions to generate fake data more close to real data
-    # model = TabularPreset(name='FAST_ML', metadata=metadata)
+    # notable issues:
+    # frequency bins don't line up with target freqs
+    # bin values in a sample should be close to each other or multiples
     model = GaussianCopula()
     model.fit(note_data)
 
@@ -21,7 +16,14 @@ def generate_data():
 
 
 if __name__ == '__main__':
-    frequencies = librosa.fft_frequencies(sr=22050, n_fft=2048)
+    ML_dir = os.path.abspath(os.curdir)
+    UPKW_file = f'{ML_dir}/UprightPianoKW_notes.csv'
+    mp3_file = f'{ML_dir}/mp3_notes.csv'
 
-    working_df = generate_data()
-    working_df.to_csv(path_or_buf=f'{os.getcwd()}/gaussian_copula_notes.csv', index=False)
+    UPKW_df = pd.read_csv(UPKW_file, header=0)
+    mp3_df = pd.read_csv(mp3_file, header=0)
+
+    combined_df = pd.concat([UPKW_df, mp3_df], ignore_index=True).iloc[:, 1:]
+
+    working_df = generate_data(combined_df)
+    working_df.to_csv(path_or_buf=f'{ML_dir}/sdv_notes.csv', index=False)
--- audio_analysis/ML/training_data.py
+++ audio_analysis/ML/training_data.py
@@ -6,13 +6,13 @@ import pandas as pd
 from audio_analysis import analyzer
 
 
-def get_sample_data(path: str, filename: str, n_bins_per_sample) -> pd.DataFrame:
+def _get_sample_data(path: str, filename: str, n_bins_per_sample: int) -> pd.DataFrame:
     S, freq_range, onsets = analyzer.get_audio_info(path)
 
     note = S[:, onsets[0]:]
-    note_data = analyzer.get_note_data(note, n_bins=100)
+    note_data = analyzer.get_note_data(note, n_bins=100, for_ML=True)
 
-    df = pd.DataFrame(note_data.T,
+    df = pd.DataFrame(note_data,
                       columns=['frequency_bin', 'max_magnitude', 'min_magnitude', 'avg_magnitude', 'sd_magnitude',
                                'avg_difference', 'sd_difference'])
     df.sort_values('avg_magnitude', inplace=True, ascending=False, ignore_index=True)
@@ -22,30 +22,32 @@ def get_sample_data(path: str, filename: str, n_bins_per_sample) -> pd.DataFrame
     final = working_df.stack(level=0).to_frame().T
     final.columns = final.columns.map(lambda x: '_'.join([str(i) for i in x]))
 
-    note_name = filename[:filename.index('v')]
+    note_name = filename[:filename.index('.')]
     target = librosa.note_to_hz(note_name)
     final['target_frequency'] = target
-    final['file_name'] = filename
 
     return final
 
 
-def format_train_data(n_bins_per_sample=10):
-    # notes in sample folders are just for testing
-    for root, dirs, files in os.walk(f'{os.getcwd()}/samples'):
-        pass
-    paths = list(map(lambda file: f'{root}/{file}', files))
+def format_train_data(train_data_dir: str, n_bins_per_sample: int) -> pd.DataFrame:
+    filenames = os.listdir(train_data_dir)
+    filepaths = list(map(lambda f: f'{train_data_dir}/{f}', filenames))
 
     frames = []
-    for i in range(len(paths)):
-        frames.append(get_sample_data(paths[i], files[i], n_bins_per_sample=n_bins_per_sample))
+    for i in range(len(filepaths)):
+        frames.append(_get_sample_data(filepaths[i], filenames[i], n_bins_per_sample=n_bins_per_sample))
 
     result = pd.concat(frames, ignore_index=True)
+    result.insert(0, 'file_name', filenames)
 
     return result
 
 
 if __name__ == '__main__':
-    data = format_train_data(n_bins_per_sample=10)
-    print(data)
-    data.to_csv(path_or_buf=f'{os.getcwd()}/notes.csv', index=False)
+    ML_dir = os.path.abspath(os.curdir)
+
+    samples_path = f'{ML_dir}/mp3_samples'
+    files = os.listdir(samples_path)
+
+    data = format_train_data(samples_path, n_bins_per_sample=10)
+    data.to_csv(path_or_buf=f'{ML_dir}/mp3_notes.csv', index=False)
